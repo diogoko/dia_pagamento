56debb94fc295a534433ef765f0a4bd3f2398b0c
Diogo Kollross <diogoko@gmail.com>
2019-07-26 21:38:51 -0300

    Extrai cálculo para módulo próprio

--- lib/dia_pagamento.ex
+++ lib/dia_pagamento.ex
@@ -1,3 +1,12 @@
 defmodule DiaPagamento do
-
+  def por_ano_mes(ano, mes) do
+    with primeiro_dia_mes <- Timex.beginning_of_month(ano, mes),
+         quinto_dia_util  <- DiasUteis.contar_dias_uteis(primeiro_dia_mes, 5),
+         dez_dias_antes   <- DiasUteis.contar_dias_uteis(Timex.shift(quinto_dia_util, [days: -1]), 10, -1) do
+      %{
+        adiantamento: dez_dias_antes,
+        pagamento: quinto_dia_util
+      }
+    end
+  end
 end
--- test/dia_pagamento_test.exs
+++ test/dia_pagamento_test.exs
@@ -1,4 +1,12 @@
+
+
 defmodule DiaPagamentoTest do
   use ExUnit.Case
   doctest DiaPagamento
+
+  test "traz os dados de 07/2019" do
+    %{adiantamento: dia_adiantamento, pagamento: dia_pagamento} = DiaPagamento.por_ano_mes(2019, 7)
+    assert Timex.equal?(dia_adiantamento, ~D[2019-06-21])
+    assert Timex.equal?(dia_pagamento, ~D[2019-07-05])
+  end
 end
