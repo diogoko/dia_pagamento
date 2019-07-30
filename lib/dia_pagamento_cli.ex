adb8d01ed80f2aab4fcad46b93281d5161e4e5c4
Diogo Kollross <diogoko@gmail.com>
2019-07-29 23:42:39 -0300

    Extrai formatação para funções menores

--- lib/dia_pagamento_cli.ex
+++ lib/dia_pagamento_cli.ex
@@ -6,10 +6,21 @@ defmodule DiaPagamento.CLI do
     1..12
     |> Enum.map(&DiaPagamento.por_ano_mes(2019, &1))
     |> Enum.map(
-      fn %{ano: _, mes: m, adiantamento: adiantamento, pagamento: pagamento} ->
-        "#{m |> Integer.to_string |> String.pad_leading(2, "0")}     #{adiantamento |> Timex.format!("%d/%m", :strftime)}      #{pagamento |> Timex.format!("%d/%m", :strftime)}"
+      fn %{ano: _, mes: mes, adiantamento: adiantamento, pagamento: pagamento} ->
+        "#{mes_formatado(mes)}     #{dia_formatado(adiantamento)}      #{dia_formatado(pagamento)}"
       end
     )
     |> Enum.map(&IO.puts/1)
+  end
+
+  defp mes_formatado(mes) do
+    mes
+    |> Integer.to_string()
+    |> String.pad_leading(2, "0")
+  end
+
+  defp dia_formatado(dia) do
+    dia
+    |> Timex.format!("%d/%m", :strftime)
   end
 end
